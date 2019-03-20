8f044a3ae0d6d3a49cd99d7b6de992ee9aef13ca
stepanvanzuriak <stepanvanzuriak@gmail.com>
2019-03-20 20:54:00 +0200

    Update configs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Develop version</p>
+          <p>Develop version 2</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
